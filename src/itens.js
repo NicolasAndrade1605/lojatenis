3cc186c58177614ccb1f3ea3c87d09d1f1a71a15
NicolasAndrade1605 <87158638+NicolasAndrade1605@users.noreply.github.com>
2023-09-06 16:42:19 -0300

    first commit

---
+++ src/itens.js
@@ -0,0 +1,20 @@
+const items = [
+    {
+      id: 0,
+      nome: 'camiseta',
+      img: 'title.png',
+      quantidade: 0
+    },
+    {
+      id: 1,
+      nome: 'short',
+      img: 'title.png',
+      quantidade: 0
+    },
+    {
+      id: 2,
+      nome: 'sapato',
+      img: 'title.png',
+      quantidade: 0
+    },
+  ]
